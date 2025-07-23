f76b499f1c190e7f5d1288c0be8391421953d0a4
sakibhasanice <hasansakib047@gmail.com>
2025-07-24 00:20:48 +0600

    app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from pyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+import google.generativeai as genai
+
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChbatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set up Google Generative AI API key
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000,
+        chunk_overlap=1000,
+    )
+    return text_splitter.split_text(text)
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+        
+
+
+
